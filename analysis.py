614abb8b8e38dc2bc186b64263d2ec8270c09507
Jonathan Guilbe <jguilbe@knights.ucf.edu>
2018-05-30 12:59:57 -0400

    Initial version of analysis code. Updated gitignore.

---
+++ analysis.py
@@ -0,0 +1,49 @@
+import test_detect
+
+
+def frange(start, stop, step):
+    x = start
+    while x < stop:
+        yield x
+        x += step
+
+report = open('DetailedReport.txt', "w")
+summary = open('Summary.txt', "w")
+summary.write('TestNum, I, A, B, Correct, FalsePositives, FalseNegatives\n')
+
+counter = 0
+
+answers = []
+
+answer_file = open('answers.txt', 'r')
+test_count = int(answer_file.readline())
+
+for answer in xrange(0, test_count):
+    hasObstacle, distance = answer_file.readline().split(' ')
+    answers.append((hasObstacle, distance))
+print(answers)
+
+for i in frange(0.2, 0.7, 0.1):
+    for a in xrange(-11, 0):
+        for b in xrange(4000, 8000, 100):
+            counter += 1
+            report.write('Test {} I:{} A:{} B:{}\n'.format(counter, i, a, b))
+            correct = 0
+            false_pos = 0
+            false_neg = 0
+            for test in xrange(0, test_count):
+                result = test_detect.detect(str(test + 1) + '.txt', i, a, b)
+                if result == bool(answers[test][0]):
+                    result_string = "Correct"
+                    correct += 1
+                elif result is True:
+                    result_string = "False Positive"
+                    false_pos += 1
+                else:
+                    result_string = "False Negative"
+                    false_neg += 1
+                report.write('{}.txt Expected: {} Actual: {} Result: {}\n'.format(str(test + 1), answers[test][0], result, result_string))
+            report.write('\n')
+            summary.write('{} {} {} {} {} {} {}\n'.format(counter, i, a, b, correct, false_pos, false_neg))
+report.close()
+summary.close()
--- test_detect.py
+++ test_detect.py
@@ -2,40 +2,29 @@ from scipy import stats
 import numpy as np
 import sys
 
-file_name = sys.argv[1]
-print(file_name)
-array = np.loadtxt(file_name)  # 20 by 20
-array = array[array != 0]
 
-indicies = np.arange(1, len(array) + 1)
-#indicies = [1]
+def detect(input_file, i_threshold, a_threshold, b_threshold):
+    # file_name = sys.argv[1]
+    # print(file_name)
+    array = np.loadtxt(input_file)
+    array = array[array != 0]
+# Need to rework ^ to work with 2d arrays!
+    indicies = np.arange(1, len(array) + 1)
 
-#print(indicies)
-#print(array[:,10])
-#print("Onwards!")
+    slope, intercept, r_value, p_value, std_err = stats.mstats.linregress(indicies, array)
 
-#print(len(indicies))
-#print(len(array[:,10]))
-#print("NOW Onwards!")
+    # print("Slope: " + str(slope))
+    # print("Intercept: " + str(intercept))
+    # print("R: " + str(r_value))
+    # print("R Squared: " + str(r_value * r_value))
+    # print("P: " + str(p_value))
+    # print("Std Error: " + str(std_err))
 
-print(len(indicies))
-print(len(array))
+    # i_threshold = 0.4
+    # a_threshold = -4
+    # b_threshold = 3000
 
-slope, intercept, r_value, p_value, std_err = stats.mstats.linregress(indicies, array)
-
-print("Slope: " + str(slope))
-print("Intercept: " + str(intercept))
-print("R: " + str(r_value))
-print("R Squared: " + str(r_value * r_value))
-print("P: " + str(p_value))
-print("Std Error: " + str(std_err))
-
-i_threshold = 0.4
-a_threshold = -4
-b_threshold = 3000
-
-if (r_value * r_value) > i_threshold and slope < a_threshold and  intercept > b_threshold:
-    print("We're clear!")
-else:
-    print("Something's in the way!")
-    
+    if (r_value * r_value) > i_threshold and slope < a_threshold and intercept > b_threshold:
+        return True
+    else:
+        return False
