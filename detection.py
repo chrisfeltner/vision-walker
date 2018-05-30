e085b4a77327e00c89f15ab4ff07cf69e6e9c717
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-05-30 14:24:30 -0400

    get_average_values

--- detection.py
+++ detection.py
@@ -4,8 +4,19 @@ import numpy as np
 from settings import I_THRESHOLD, A_THRESHOLD, B_THRESHOLD
 
 class Detector(object):
-    def get_average_values(width, array):
-        
+    def get_average_values(width_for_avg, array):
+        height, width = np.shape(array)
+        center = width/2
+        starting_width_index = center - (width_for_avg/2)
+        ending_width_index = center + (width_for_avg/2)
+        average_array = np.zeros(height)
+        for y in range(0, height):
+            sum = 0
+            for x in range(starting_width_index, ending_width_index):
+                sum += array[y][x]
+            average_array[y] = sum/(ending_width_index - starting_width_index)
+        return average_array
+
 
     # Detects obstables by identifying non-linear distance
     # Returns True if an obstacle is detected
