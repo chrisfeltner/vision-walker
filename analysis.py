9273f471d617ea00d4196c0692981edb3d4894a4
Jonathan Guilbe <jguilbe@knights.ucf.edu>
2018-06-04 15:50:37 -0400

    Cleaned up some stuff, added comments.

--- analysis.py
+++ analysis.py
@@ -1,6 +1,8 @@
 import test_detect
 
 
+# For the Correlation coefficient, we want to step through a range
+# using floats. This isn't a native thing in Python.
 def frange(start, stop, step):
     x = start
     while x < stop:
@@ -8,30 +10,35 @@ def frange(start, stop, step):
         x += step
 
 
+# Open related files, then give Summary a header.
 report = open('DetailedReport.txt', "w")
 summary = open('Summary.txt', "w")
 summary.write('TestNum, I, A, B, Correct, FalsePositives, FalseNegatives\n')
 
+# Keeps track of which combination we're on.
 counter = 0
-
+# Stores the information from the answers text file
 answers = []
 
-
+# Open the answer file, then grab the first line (the # of test files)
 answer_file = open('answers.txt', 'r')
 test_count = int(answer_file.readline())
 
+# Keep track of correct and incorrect #s for each test file
 correct_array = [0] * test_count
 falsepos_array = [0] * test_count
 falseneg_array = [0] * test_count
 
+# Read in answers, split it away from the measured distance.
 for answer in xrange(0, test_count):
     hasObstacle, distance = answer_file.readline().split(' ')
     answers.append((hasObstacle, distance))
-print(answers)
 
+# For loop over correlation coefficient, slope, and y-intercept
 for i in frange(0.2, 0.8, 0.1):
     for a in xrange(-11, 0):
         for b in xrange(2000, 9000, 1000):
+            # Store this combination's statistics for later writing
             counter += 1
             report.write('Test {} I:{} A:{} B:{}\n'.format(counter, i, a, b))
             correct = 0
@@ -56,6 +63,8 @@ for i in frange(0.2, 0.8, 0.1):
             report.write('\n')
             summary.write('{} {} {} {} {} {} {}\n'.format(
                 counter, i, a, b, correct, false_pos, false_neg))
+
+# Write out each test file's stats.
 report.write("File Results")
 for i in xrange(0, 10):
     report.write("{}.txt Correct: {} False Positive: {} False Negative: {}".format(
--- test_detect.py
+++ test_detect.py
@@ -4,8 +4,6 @@ import sys
 
 
 def detect(input_file, i_threshold, a_threshold, b_threshold):
-    # file_name = sys.argv[1]
-    # print(file_name)
     array = np.loadtxt(input_file)
     # Temporary fix for the 2d array stuff. In practice we wouldn't need this?
     if len(np.shape(array)) > 1:
@@ -13,23 +11,12 @@ def detect(input_file, i_threshold, a_threshold, b_threshold):
         array = array[:, width / 2]
     array = array[array != 0]
     if len(array) == 0:
-        return True  # For now we can assume super close object filling screen = bad
-# Need to rework ^ to work with 2d arrays?
-    indicies = np.arange(1, len(array) + 1)
+        # For now we can assume super close object filling screen = bad
+        return True
 
+    indicies = np.arange(1, len(array) + 1)
     slope, intercept, r_value, p_value, std_err = stats.mstats.linregress(
         indicies, array)
-
-    # print("Slope: " + str(slope))
-    # print("Intercept: " + str(intercept))
-    # print("R: " + str(r_value))
-    # print("R Squared: " + str(r_value * r_value))
-    # print("P: " + str(p_value))
-    # print("Std Error: " + str(std_err))
-
-    # i_threshold = 0.4
-    # a_threshold = -4
-    # b_threshold = 3000
 
     if (r_value * r_value) > i_threshold and slope < a_threshold and intercept < b_threshold:
         return False  # NO OBJECT DETECTED
