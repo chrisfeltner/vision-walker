ff671c8a4bcf5bfb4d3b63a2d1dc5358346d1aac
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-06-26 15:54:24 -0400

    passThroughFilter

--- VisionWalker.cpp
+++ VisionWalker.cpp
@@ -1,6 +1,7 @@
 #include <VisionWalker.h>
-#include <pcl/filters/voxel_grid.h>
 
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
 
 VisionWalker::VisionWalker()
 {
@@ -15,9 +16,26 @@ pcl::PCLPointCloud2::Ptr createVoxelGrid(pcl::PCLPointCloud2::Ptr cloudToFilter 
     }
 
     pcl::PCLPointCloud2::Ptr filteredCloud = pcl::PCLPointCloud2();
-    pcl::VoxelGrid<pcl::PCLPointCloud2> voxelFilter;
+    pcl::VoxelGrid<pcl::PointXYZ> voxelFilter;
     voxelFilter.setInputCloud(cloudToFilter);
     voxelFilter.setLeafSize(0.01f, 0.01f, 0.01f);
     voxelFilter.filter(*filteredCloud);
+    return filteredCloud;
+}
+
+pcl::PCLPointCloud2::Ptr runPassThroughFilter(pcl::PCLPointCloud2::Ptr cloudToFilter = NULL,
+    char *field, double min, double max)
+{
+    if(cloudToFilter == NULL)
+    {
+        return NULL;
+    }
+
+    pcl::PCLPointCloud2::Ptr filteredCloud = pcl::PCLPointCloud2();
+    pcl::PassThrough<pcl::PointXYZ> passThroughFilter;
+    passThroughFilter.setInputCloud(cloudToFilter);
+    passThroughFilter.setFilterFieldName(field);
+    passThroughFilter.setFilterLimits(min, max);
+    passThroughFilter.filter(*filteredCloud);
     return filteredCloud;
 }
--- VisionWalker.h
+++ VisionWalker.h
@@ -9,7 +9,8 @@ class VisionWalker
         VisionWalker();
 
         pcl::PCLPointCloud2::Ptr createVoxelGrid(pcl::PCLPointCloud2::Ptr cloudToFilter = NULL);
-        pcl::PCLPointCloud2::Ptr passThroughFilter(pcl::PCLPointCloud2::Ptr cloudToFilter = NULL);
+        pcl::PCLPointCloud2::Ptr runPassThroughFilter(pcl::PCLPointCloud2::Ptr cloudToFilter = NULL,
+            char* field, double min, double max);
 
     private:
 };
