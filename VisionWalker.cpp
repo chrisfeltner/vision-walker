b5b0c8b9cf78488d819e532f3331f33c34010f61
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-06-28 16:00:25 -0400

    cast pclPointcloud to pclpointcloud2

--- VisionWalker.cpp
+++ VisionWalker.cpp
@@ -39,7 +39,9 @@ void VisionWalker::process(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud
     if(!viewer->wasStopped())
     {
         pcl::PointCloud<pcl::PointXYZ>::Ptr passThroughCloud = runPassThroughFilter(cloud, "z", 0.4, 4.0);
-        pcl::PCLPointCloud2::Ptr voxelCloud = createVoxelGrid(passThroughCloud);
+        pcl::PCLPointCloud2::Ptr passThroughCloud2;
+        pcl::toPCLPointCloud2(passThroughCloud, passThroughCloud2);
+        pcl::PCLPointCloud2::Ptr voxelCloud = createVoxelGrid(passThroughCloud2);
         viewer->showCloud(voxelCloud);
     }
     
