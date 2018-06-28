84f8abf5e9f79342d6386360cf8c384361300b92
Christopher Feltner <chris.feltner@knights.ucf.edu>
2018-06-28 15:24:05 -0400

    typo fix

--- VisionWalker.cpp
+++ VisionWalker.cpp
@@ -56,7 +56,7 @@ void VisionWalker::run()
 
     while(!viewer.wasStopped())
     {
-        boost::this_thread::sleep(boost:posix_time::seconds(1));
+        boost::this_thread::sleep(boost::posix_time::seconds(1));
     }
 
     knightsWhoGrabNi->stop();
@@ -64,7 +64,7 @@ void VisionWalker::run()
 
 int main(int argc, char const *argv[])
 {
-    VisionWalker() vw;
+    VisionWalker vw;
     vw.run();
     return 0;
 }
