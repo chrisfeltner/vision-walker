f140917f8a414925de275d6c1de070120c3e9538
Jonathan Guilbe <jguilbe@knights.ucf.edu>
2018-07-06 17:16:16 -0400

    ptr

--- VisionWalker.h
+++ VisionWalker.h
@@ -1,7 +1,7 @@
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/PCLPointCloud2.h>
-#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/cloud_viewer.h>
 
 #ifndef VISION_WALKER
 #define VISION_WALKER
@@ -11,21 +11,21 @@ class VisionWalker
     public:
     VisionWalker()
     {
-        viewer = new pcl::visualization::PCLVisualizer ("PCL OpenNI Viewer");
+        viewer = new pcl::visualization::CloudViewer("PCL OpenNI Viewer");
     }
 
     pcl::PCLPointCloud2::Ptr createVoxelGrid(pcl::PCLPointCloud2::Ptr cloudToFilter);
     pcl::PointCloud<pcl::PointXYZ>::Ptr runPassThroughFilter(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloudToFilter, char *field, double min, double max);
-    bool segmentFloorPlane(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloudToSegment, pcl::ModelCoefficients::Ptr coefficients, pcl::PointIndices::Ptr inliers);
-    std::vector<pcl::PointIndices> segmentWallPlanes(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloudToSegment);
+    void segmentFloorPlane(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloudToSegment, pcl::ModelCoefficients::Ptr coefficients, pcl::PointIndices::Ptr inliers);
+    std::vector<pcl::PointIndices::Ptr> segmentWallPlane(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloudToSegment);
     pcl::PointCloud<pcl::PointXYZ>::Ptr extractPoints(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, pcl::PointIndices::Ptr inliers);
     void run();
     void process(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr &cloud);
 
     private:
-    pcl::visualization::PCLVisualizer* viewer;
+    pcl::visualization::CloudViewer* viewer;
     static const int INLIER_THRESHOLD = 20000;
-    static const float RANSAC_DISTANCE_THRESHOLD = 0.1;
+    static const float RANSAC_DISTANCE_THRESHOLD = 0.03;
     static const float MINIMUM_X = -0.4;
     static const float MAXIMUM_X = 0.4;
     static const float MINIMUM_Z = 0.4;
